0eeaada50fc4d1fe9fbf2cae661ea37fc7086d0c
Spike Grobstein <spikegrobstein@mac.com>
2011-02-07 14:53:56 -0500

    added some placeholder graphics for the answer... sorta implemented that stuff.

--- jess_vday.js
+++ jess_vday.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 	// for now, let's just click the 4th panel and have it do its thing
 	
 	$('#right_panel').click(function() {
+		// update the background
+		$('#comic').css({'background-image': 'url(images/yes_bg.png)'});
 		$(this).animate({ top: '550px' });
 	});
 });
